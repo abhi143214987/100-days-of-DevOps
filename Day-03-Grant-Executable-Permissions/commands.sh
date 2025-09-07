689a4c21eac364b65dd4c3a7284f5d0b59c14431
abhi <abhilashrathod246@gmail.com>
2025-09-07 16:39:27 +0530

    add script

---
+++ Day-03-Grant-Executable-Permissions/commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Day 03 – Grant Executable Permissions to a Script
+
+# Step 1: Grant execute permissions to the script for all users
+chmod a+x /tmp/xfusioncorp.sh
+
+# Step 2: Verify the permissions
+ls -l /tmp/xfusioncorp.sh
+
